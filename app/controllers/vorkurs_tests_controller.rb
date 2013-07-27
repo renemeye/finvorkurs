cf23631ac26da6da07c406d3eaa7ef9668d8efae
René Meye <rene@meye.md>
2013-07-27 23:45:21 +0200

    Refectoring of Test to VorkursTest
    Signed-off-by: René Meye <rene@meye.md>

--- app/controllers/vorkurs_tests_controller.rb
+++ app/controllers/vorkurs_tests_controller.rb
@@ -3,17 +3,17 @@ before_filter :authenticate_user!
 
   def show
     @user = current_user
-    @test = Test.find params[:id]
+    @test = VorkursTest.find params[:id]
   end
 
   def new
     @user = current_user
-    @test = Test.find params[:id]
+    @test = VorkursTest.find params[:id]
   end
 
   def index
     @user = current_user
-    @test = Test.find params[:id]
+    @test = VorkursTest.find params[:id]
     correct = @user.answers_for(@test).reduce(0) { |a, e| a += e.correct ? 1 : 0 }
     @score = correct * 100 / @test.questions.count
     @test_result = @test.test_results.create
