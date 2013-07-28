02a5ff836e49b2b68ebc98bac7099149a040be66
René Meye <rene@meye.md>
2013-07-28 04:10:31 +0200

    View false and right message on Questions
    Signed-off-by: René Meye <rene@meye.md>

--- app/models/question.rb
+++ app/models/question.rb
@@ -10,4 +10,24 @@ class Question < ActiveRecord::Base
   def next
     self.class.first :order => 'id', :conditions => ['id > ?', self.id]
   end
+
+  def answered_by? user
+  	user.answers.each do |answer|
+  		return true if answer.question_id == self.question_id
+  	end
+  	return false
+  end
+
+  def correct_answer_from? user
+  	correct = true
+  	answered_the_question = false
+
+  	user.answers.each do |answer|
+  		if answer.question_id == self.id
+  			answered_the_question = true
+  			correct = false unless answer.correct
+  		end
+  	end
+  	return answered_the_question && correct
+  end
 end
