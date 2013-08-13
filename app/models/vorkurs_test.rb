1cfc45cadbde4f7c12c54772a50f6ff01f776949
René Meye <rene@meye.md>
2013-08-13 21:33:30 +0200

    Randomize Questions
    Signed-off-by: René Meye <rene@meye.md>

--- app/models/vorkurs_test.rb
+++ app/models/vorkurs_test.rb
@@ -6,7 +6,28 @@ class VorkursTest < ActiveRecord::Base
    has_many :test_results
 
    def resume_test_path user
-    next_question = (self.questions - user.questions).first
-    next_question && [self, next_question]
+
+   	if Settings.vorkurs_test.randomized_questioning
+	   	test_questions = self.questions
+	   	test_questions_count = self.questions.count
+	   	answered_questions = test_questions & user.questions
+
+	   	# Answered enough questions?
+	   	max_needed = [test_questions_count, Settings.vorkurs_test.max_questions_per_test].min
+		return false if answered_questions.count >= max_needed
+
+		# Find the first unanswerd question
+		static_random_order = user.static_randoms(self.id, test_questions_count*test_questions_count, min = 0, test_questions_count-1, 0).uniq
+		counter=0
+		next_question = test_questions[static_random_order[counter]]
+		while next_question.answered_by?(user)
+			next_question = test_questions[static_random_order[counter]]
+			counter += 1;
+		end
+	else
+	    next_question = (self.questions - user.questions).first
+	end
+
+	next_question && [self, next_question]
   end
 end
