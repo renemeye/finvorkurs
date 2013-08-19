de6e355e711e8c2679e866781f1736bf668e90ff
René Meye <rene@meye.md>
2013-08-19 16:38:21 +0200

    Added first Tests
    Signed-off-by: René Meye <rene@meye.md>

---
+++ spec/support/mailer_macros.rb
@@ -0,0 +1,14 @@
+module MailerMacros
+	def last_email
+		ActionMailer::Base.deliveries.last
+	end
+
+	def reset_email
+		ActionMailer::Base.deliveries = []
+	end
+
+	def last_emails
+		ActionMailer::Base.deliveries
+	end
+
+end
