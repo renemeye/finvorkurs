f0148e5909ee2e89539efbfb7d0972b1dcab9fa1
René Meye <rene@meye.md>
2013-08-20 11:51:32 +0200

    Nicer Welcome Message on HowTo Page
    Signed-off-by: René Meye <rene@meye.md>

---
+++ spec/requests/howto_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper.rb'
+
+describe "howto" do
+
+  describe "login_logout" do
+    it "is possible to login and logout" do
+      user = create(:user)
+
+      visit root_url
+      page.should have_content("Neu Registrieren")
+
+      login_as user
+      visit root_url
+      page.should have_content(user.email)
+
+      logout_user
+      visit root_url
+      page.should_not have_content(user.email)
+    end
+  end
+
+  it "shows Hallo only for registered user with name" do
+  	user = create(:user)
+    login_as user
+
+    visit root_url
+    page.should_not have_content("Neu Registrieren")
+    page.should have_content("Hallo,")
+
+    a_name = "Paul Riegel"
+
+    user.name = a_name
+    user.save
+    visit root_url
+    page.should have_content("Hallo #{a_name},")
+  end
+end
