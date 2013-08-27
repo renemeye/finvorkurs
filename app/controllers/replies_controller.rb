09c2e586be06a5d74b9a27bfce3f77ceeb2e7380
René Meye <rene@meye.md>
2013-08-27 10:49:35 +0200

    Fixed wrong storing of answers
    Signed-off-by: René Meye <rene@meye.md>

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -17,9 +17,9 @@ class RepliesController < ApplicationController
   		redirect_to (question.vorkurs_test.resume_test_path(user) || vorkurs_test_path(question.vorkurs_test_id)), notice: "Sie müssen mindestens eine der Antworten abgeben."
   		return
   	else
-      #WARNING: this should be used only here, because we expect, that the system don't change (now/deleted answers or Settings like maximum answers) to much in between
-      question.answers_presented_to_user(user).each do |answer|
-        Reply.create!(user: user, answer: answer, voted_as_correct:(params[:answer_ids].include?(answer.id)))
+        #WARNING: this should be used only here, because we expect, that the system don't change (now/deleted answers or Settings like maximum answers) to much in between
+        question.answers_presented_to_user(user).each do |answer|
+          Reply.create!(user: user, answer: answer, voted_as_correct:(params[:answer_ids].include?(answer.id.to_s)))
       end
     end
     
