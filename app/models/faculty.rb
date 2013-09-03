31486865c4bfc1d695309bdf96b847b153894f86
René Meye <rene@meye.md>
2013-09-03 03:50:26 +0200

    Fixed Faculty.users having duplicate entries
    Signed-off-by: René Meye <rene@meye.md>

--- app/models/faculty.rb
+++ app/models/faculty.rb
@@ -1,6 +1,6 @@
 class Faculty < ActiveRecord::Base
 	attr_accessible :name, :short_name, :degree_programs_attributes
 	has_many :degree_programs
-	has_many :users, :through => :degree_programs
+	has_many :users, :through => :degree_programs, :uniq => true
   	accepts_nested_attributes_for :degree_programs, allow_destroy: true
 end
