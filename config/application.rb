3c9a4db9001ab386222e87832e8dcb29e64497e8
René Meye <rene@meye.md>
2013-09-08 15:06:28 +0200

    Enable SSL for Server
    This schould close #81
    
    Signed-off-by: René Meye <rene@meye.md>

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ end
 
 module FinVorkurs
   class Application < Rails::Application
+
+    config.force_ssl = true if (ENV['SSL'] == "yes")
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
