beeeac9c12a88eacf06747c03fd3779d84282c58
René Meye <rene@meye.md>
2013-09-10 15:58:08 +0200

    Markdown Support for Course Description
    Signed-off-by: René Meye <rene@meye.md>

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,6 +5,17 @@ class Course < ActiveRecord::Base
   has_many :users, through: :enrollments
   has_many :lectures
 
+  @@markdown_options = [
+      :autolink=>true, 
+      :disable_indented_code_blocks=>true, 
+      :no_intra_emphasis=>true,
+      :strikethrough=>true,
+      :underline=>true,
+      :lax_spacing => true,
+      :hard_wrap=>true
+  ]
+  @@markdown = Redcarpet::Markdown.new(MathjaxCompatibleMarkdown.new(*@@markdown_options), *@@markdown_options)
+
   def course_name
   	return (self.course_level == self.title)?
   		self.title :
@@ -17,6 +28,10 @@ class Course < ActiveRecord::Base
 
   def self.by_level
     return Course.all.group_by{|c|c.course_level}
+  end
+
+  def markdown_description
+    @@markdown.render(self.description)
   end
 
 end
