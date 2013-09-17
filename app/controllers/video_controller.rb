1a16029758d9fbb1370f432eec86e9740a414f7c
René Meye <rene@meye.md>
2013-09-17 11:52:43 +0200

    Create and Destroy action
    Signed-off-by: René Meye <rene@meye.md>

--- app/controllers/video_controller.rb
+++ app/controllers/video_controller.rb
@@ -5,4 +5,23 @@ class VideoController < ApplicationController
 
 		@manifest = @video.catch_manifest_data
 	end
+
+  def create
+  	authenticate_admin!
+
+  	Video.create(params[:video])
+  	redirect_to action: :index, controller: :download
+  end
+
+  def destroy
+    authenticate_admin!
+    
+    video=Video.find(params[:id])
+    if video && video.destroy
+      flash[:notice] = "Video gelöscht."
+      redirect_to action: :index 
+    else
+      redirect_to action: :index
+    end
+  end
 end
