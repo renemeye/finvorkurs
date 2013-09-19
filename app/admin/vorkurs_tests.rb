cc1241737a6ea989f146aec4a578240f3d4ea2de
René Meye <rene@meye.md>
2013-09-19 11:10:15 +0200

    Moved Histogram from Dashboard to Tests page
    This solves #98
    
    Signed-off-by: René Meye <rene@meye.md>

--- app/admin/vorkurs_tests.rb
+++ app/admin/vorkurs_tests.rb
@@ -1,5 +1,38 @@
 #encoding: utf-8
 ActiveAdmin.register VorkursTest do
+
+	index do
+		selectable_column
+    	id_column
+    	column :name
+    	column :description
+    	default_actions
+
+		 panel "Test results" do
+          h5 "Test result Histogram:"
+          test_result_histogram = VorkursTest.all.collect do |vorkurs_test|
+            counter = [[100,0],[90,0],[80,0],[70,0],[60,0],[50,0],[40,0],[30,0],[20,0],[10,0]]
+            User.all.each do |user|
+              if user.completed_test? vorkurs_test
+                result = vorkurs_test.result user
+               # if result > 0
+                  counter.each do |c|
+                    if result >= c[0]
+                      c[1] = c[1]+1
+                      break
+                    end
+                  end
+                #end
+              end
+            end
+            counter.reverse!
+          end
+
+
+          para raw "<div  style=\"height: 300px; width:100%\" class=\"multiple_category_vizualisation\" data-bars='#{test_result_histogram.to_json}'></div>"
+        end
+	end
+
 	show do |vorkurs_test|
 		attributes_table do
 	        row :id
